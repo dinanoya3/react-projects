82cc44370a53284f094f6c6638f4d5635822f83f
Dina Noya <dinanoya3@gmail.com>
2022-07-29 12:24:03 -0400

    toggle functionality to dark mode project and local storage

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
+
+// local storage
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  // if key exists, set variable to value returned
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  // change every time theme changes, and by default (when app loads)
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          {
+            /* use spread attribute to pass whole props object*/
+          }
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from "react";
+import moment from "moment";
 
-export default Article
+const Article = ({ title, snippet, date, length }) => {
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      <div className="post-info">
+        {/* need to format date,use moment.js*/}
+        <span>{moment(date).format("ddd Do MMM, YYYY")}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
+
+export default Article;
