e69bf5729b6430fc41b5afbefa9ecfcf74d6808a
Dina Noya <dinanoya3@gmail.com>
2022-07-29 17:50:23 -0400

    final code with using useFetch custom hook

--- 21-movie-db/setup/src/SearchForm.js
+++ 21-movie-db/setup/src/SearchForm.js
@@ -1,7 +1,22 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-const SearchForm = () => {
-  return <h2>search component</h2>
-}
+import React from "react";
+import { useGlobalContext } from "./context";
 
-export default SearchForm
+const SearchForm = () => {
+  const { query, setQuery, error } = useGlobalContext();
+  return (
+    // use onSubmit so page doesnt refresh when user clicks enter/submit
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>search movies</h2>
+      {/* controlled input (use state value coming from context not local state value) */}
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className="error">{error.msg}</div>}
+    </form>
+  );
+};
+
+export default SearchForm;
